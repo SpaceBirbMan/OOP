f413c113b4d151a6358f5ffae525ccc62e8905b1
SpaceBirB <126468286+SpaceBirbMan@users.noreply.github.com>
2023-10-25 21:31:40 +0900

    Update and rename Class.cpp to Class/Class.cpp

--- Class/Class.cpp
+++ Class/Class.cpp
@@ -1,36 +1,37 @@
 ﻿#include <iostream>
 #include <string>
-#include "C:\Users\kiril\OneDrive\Рабочий стол\Библиотека\Matrix.h"
+#include "C:\Users\kiril\OneDrive\Рабочий стол\Библиотека\Matrix.h" //путь к сторонней библиотеке
 
 using namespace std;
 
 int main()
 {
-	class LRS
+	class LRS //класс local radio station
 	{
 	private:
-		string sname = "";
-		float sfrequency = 0.0;
-		string sinfo = "";
-		bool sonline_status = false;
-		float scov_radius = 0.0;
-		matrix slocation;
+		string sname = ""; //название
+		float sfrequency = 0.0; //частота вещания
+		string sinfo = ""; //информация
+		bool sonline_status = false; //статус вещания
+		float scov_radius = 0.0; //радиус зоны вещания
+		matrix slocation; //местоположение (координаты на плоскости)
 		
 	public:
-		LRS(string nm = "Untitled", float freq = 0.0, float rad = 1.0, matrix loc = (0, 0))
+		LRS(string nm = "Untitled", float freq = 0.0, float rad = 1.0, matrix loc = (0, 0)) //конструктор
 		{
 			sname = nm;
 			sfrequency = freq;
 			scov_radius = rad;
 			slocation = loc;
 		}
-		void set_name(string nm) { sname = nm; }
+	//сеттеры для всех значений
+		void set_name(string nm) { sname = nm; } 
 		void set_freq(float fr) { sfrequency = fr; }
 		void set_info(string inf) { sinfo = inf; }
 		void set_stat(bool s) { sonline_status = s; }
 		void set_rad(float r) { scov_radius = r; }
 		void set_loc(matrix l) { slocation = l; }
-
+	//геттеры для всех значений
 		string get_name() { return sname; }
 		float get_freq() { return sfrequency; }
 		string get_info() { return sinfo; }
@@ -38,14 +39,14 @@ int main()
 		float get_rad() { return scov_radius; }
 		matrix get_loc() { return slocation; }
 
-		void switch_stat() 
+		void switch_stat() //переключатель вещания
 		{ 
 			if (sonline_status) 
 				sonline_status = false; 
 			else 
 				sonline_status = true; 
 		}
-		string get_all_info() 
+		string get_all_info() //вывод всей информации
 		{
 			string stat, loc = "";
 			if (sonline_status) stat = "Online";
@@ -60,6 +61,6 @@ int main()
 				+ tos(slocation) + '\n';
 		}
 		
-		~LRS();
+		~LRS(); //уничтожение станции
 	};
 }
